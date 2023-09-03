599cdf75005841bda203e74f798f9c71d3864847
hodat <hodat@gmail.com>
2023-09-04 00:08:15 +0700

    ...

--- rest_API_project/settings.py
+++ rest_API_project/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'djoser',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
-    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -57,37 +56,44 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
 ]
-DJOSER = {
-        'LOGIN_FIELD':'email',
-        'USER_CREATE_PASSWORD_RETYPE':True,
-        'USERNAME_CHANGED_EMAIL_CONFIRMATION': True,
-        'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
-        'SEND_CONFIRMATION_EMAIL':True,
-        'SET_USERNAME_RETYPE':True,
-        'SET_PASSWORD_RETYPE':True,
-        'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
-        'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
-        'ACTIVATION_URL': 'activate/{uid}/{token}',
-        'SEND_ACTIVATION_EMAIL': True,
-        'SERIALIZERS': {
-            
-        },
-}
+# DJOSER = {
+#     'LOGIN_FIELD': 'email',
+#     'USER_CREATE_PASSWORD_RETYPE': True,
+#     'USERNAME_CHANGED_EMAIL_CONFIRMATION': True,
+#     'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
+#     'SEND_CONFIRMATION_EMAIL': True,
+#     'SET_USERNAME_RETYPE': True,
+#     'SET_PASSWORD_RETYPE': True,
+#     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+#     'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
+#     'ACTIVATION_URL': 'activate/{uid}/{token}',
+#     'SEND_ACTIVATION_EMAIL': True,
+#     'SOCIAL_AUTH_TOKEN_STRATEGY': 'djoser.social.token.jwt.TokenStrategy',
+#     'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': ['http://localhost:8000/google', 'http://localhost:8000/facebook'],
+#     'SERIALIZERS': {
+#         'user_create': 'accounts.serializers.UserCreateSerializer',
+#         'user': 'accounts.serializers.UserCreateSerializer',
+#         'current_user': 'accounts.serializers.UserCreateSerializer',
+#         'user_delete': 'djoser.serializers.UserDeleteSerializer',
+#     }
+# }
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated'
+#     ],
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ),
+# }
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
 }
 
-AUTH_USER_MODEL = 'accounts.UserAccount'
+# AUTH_USER_MODEL = 'accounts.UserAccount'
 
-EMAIL_BACKEND = 'django.core.mail.backends.stmp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'datthanhho2806@gmail.com'
@@ -122,7 +128,7 @@ WSGI_APPLICATION = 'rest_API_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'NoteApp',
+        'NAME': 'noteapp',
         'USER': 'root',
         'PASSWORD': 'mysqlserver',
         'PORT': 3306,
